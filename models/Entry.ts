1d7ce0375f66e7dc61026365ba5df1f8895cc9b2
to1edo <cltr996@gmail.com>
2023-06-03 18:01:21 -0400

    add Entry Model

---
+++ models/Entry.ts
@@ -0,0 +1,26 @@
+import mongoose, {Schema,Model} from 'mongoose';
+import { Entry } from '../interfaces';
+
+interface Ientry extends Entry{
+}
+
+const entrySchema = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'in-progress', 'finished'],
+    message: 'Status must be pending, in-progress or finished',
+    required: true
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now
+  }
+})
+
+const EntryModel: Model<Ientry> = mongoose.models.EntryModel || mongoose.model('Entry', entrySchema)
+
+export default EntryModel
