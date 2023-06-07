92052cbc95ee637ec0c859d035314997d3c8645b
to1edo <cltr996@gmail.com>
2023-06-06 22:46:28 -0400

    add middleware

---
+++ pages/api/bad-request.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type Data={
+  message:string|string[]
+}
+export default function handler(req:NextApiRequest, res:NextApiResponse<Data>) {
+
+  const { message = 'Bad Request'} = req.query;
+  
+  res.status(400).json({ message });
+}
